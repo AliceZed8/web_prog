2de9dbbcbf5394a03dcd88740d3b1ea296eeac45
Alice_Zed <zokerfeed@gmail.com>
2024-03-01 09:32:48 +0200

    index.php updated. lab2 p1 added

--- lab 1 Local Docker/code/index.php
+++ lab 1 Local Docker/code/index.php
@@ -1,6 +1,14 @@
 <?php
-echo phpinfo();
+// #1 Access by link
+echo "#1 Access by link\n";
 
+$very_bad_unclear_name = "15 chicken wings";
 
+// Объявляем $order как ссылку
+$order = &$very_bad_unclear_name;
 
+$order .= " some text";
+echo "Your order is: $very_bad_unclear_name.\n";
+
+//#######################################################
 
