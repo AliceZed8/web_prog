6bd2634ea650f4468ceaee47a8fd786c38aca97d
Alice_Zed <zokerfeed@gmail.com>
2024-03-01 09:18:30 +0200

    updated index.php

--- lab 1 Local Docker/code/index.php
+++ lab 1 Local Docker/code/index.php
@@ -1,14 +1,6 @@
 <?php
-// #1
-/* Imagine a lot of code here */ 
-$very_bad_unclear_name = "15 chicken wings";
+echo phpinfo();
 
-// Write your code here:
-$order = &$very_bad_unclear_name;
 
-$order .= " some text";
-  
-// Don't change the line below
-echo "\nYour order is: $very_bad_unclear_name.";
 
-// OUT: "Your order is: 15 chicken wings some text."
+
