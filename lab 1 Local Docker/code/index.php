e4f9439d4bd8a67dce355ab3171e4b151b778b75
Alice_Zed <zokerfeed@gmail.com>
2024-03-01 11:35:16 +0200

    index.php updated. lab2 p16 added

--- lab 1 Local Docker/code/index.php
+++ lab 1 Local Docker/code/index.php
@@ -192,3 +192,60 @@ $my_num = printStringReturnNumber();
 echo $my_num."\n\n";
 
 //###########################################################
+
+// #16 functions
+printf("#16  functions\n");
+
+function increaseEnthusiasm(string $str) : string {
+    $str .= "!";
+    return $str;
+}
+
+echo increaseEnthusiasm("Hello")."\n";
+
+function repeatThreeTimes(string $str) : string {
+
+    return str_repeat($str, 3);
+}
+
+echo repeatThreeTimes("Meow")."\n";
+
+echo increaseEnthusiasm(repeatThreeTimes("Woof"))."\n";
+
+function cut(string $str, int $m, int $n = 10): string {
+    return substr($str, 0 , $m);
+}
+
+$arr = [];
+for ($i = 0; $i < 10; $i++) {
+    $arr[] = rand(1, 100);
+}
+
+function print_arr(Array $arr, int $start_index) {
+    if ($start_index > count($arr) - 1) return;
+
+    echo $arr[$start_index]. " ";
+    print_arr($arr, ++$start_index);
+}
+
+echo "Массив: ";
+print_arr($arr, 0);
+echo "\n";
+
+#Дано число
+$num = 465;
+
+function isSumLess(int $num): int {
+    $sum = 0;
+    $arr = str_split((string) $num);
+    foreach($arr as $k => $v) {
+        $sum += (int) $v;
+    }
+
+    if ($sum > 9) return isSumLess($sum);
+    else return $sum;
+}
+
+
+//###########################################################
+
