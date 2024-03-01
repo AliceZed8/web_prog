7dd503a7f80ec421e2cffb431787676a5cdcb4c1
Alice_Zed <zokerfeed@gmail.com>
2024-03-01 09:47:06 +0200

    index.php updated. lab2 p2 added

--- lab 1 Local Docker/code/index.php
+++ lab 1 Local Docker/code/index.php
@@ -1,6 +1,6 @@
 <?php
 // #1 Access by link
-echo "#1 Access by link\n";
+printf("#1 Access by link\n");
 
 $very_bad_unclear_name = "15 chicken wings";
 
@@ -8,7 +8,31 @@ $very_bad_unclear_name = "15 chicken wings";
 $order = &$very_bad_unclear_name;
 
 $order .= " some text";
-echo "Your order is: $very_bad_unclear_name.\n";
+echo "Your order is: $very_bad_unclear_name.\n\n";
 
 //#######################################################
 
+// #2 Numbers
+printf("#2 Numbers\n");
+
+$i = 0;
+echo $i;
+echo "\n";
+
+$j = 1;
+echo $j;
+echo "\n";
+
+$double = 3.1415926535;
+echo $double;
+echo "\n";
+
+echo 3 << 2; // Бит сдвиг
+echo "\n";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+
+echo "Diff ". $last_month - $this_month."\n\n";
+
+//#######################################################
