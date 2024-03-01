bc2612c123d2d410508c310d4fe4d55d77d9a378
Alice_Zed <zokerfeed@gmail.com>
2024-03-01 12:18:42 +0200

    index.php updated. lab2 p18 added

--- lab 1 Local Docker/code/index.php
+++ lab 1 Local Docker/code/index.php
@@ -329,3 +329,47 @@ $l = count($arr);
 echo $arr[$l - 1]." ".$arr[$l - 2]."\n";
 
 //###########################################################
+
+// #18 if/else
+printf("#18  if/else\n");
+
+#1
+function f1(int $a, $b): bool {
+    if ($a + $b > 10) return true;
+    else return false;
+}
+
+#2
+function f2(int $a, $b): bool {
+    if ($a == $b) return true;
+    else return false;
+}
+
+#3
+$test = 0;
+if (!$test) echo "верно\n";
+
+#4
+$age = 19;
+
+if ($age < 10 || $age > 99)
+    echo "Возраст меньше 10 или больше 99\n";
+else {
+    $sum = 0;
+    $arr = str_split((string) $age);
+    foreach ($arr as $k => $v) {
+        $sum += (int)$v;
+    }
+    if ($sum <= 9) echo "Сумма цифр однозначна\n";
+    else echo "Сумма цифр двузначна\n";
+}
+
+#5
+
+$arr = [1 , 3, 4];
+
+if (count($arr) == 3) {
+    echo "Сумма элементов: ".array_sum($arr)."\n";
+}
+
+//###########################################################
