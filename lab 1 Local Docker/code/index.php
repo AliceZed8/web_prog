31e15c6f324259ae37bb321b0cca37f179eb6f40
Alice_Zed <zokerfeed@gmail.com>
2024-03-01 12:41:23 +0200

    index.php updated. lab2 p20 added

--- lab 1 Local Docker/code/index.php
+++ lab 1 Local Docker/code/index.php
@@ -388,3 +388,32 @@ for ($i = 0; $i < 20; $i++) {
 echo "\n";
 
 //###########################################################
+
+
+// #20 functions comb
+printf("#20 functions comb\n");
+
+#1
+$arr = [1, 2, 3, 5, 1, 13, 5,6, 9];
+$c = array_sum($arr) / count($arr);
+
+#2
+$res = array_sum(range(1, 100));
+
+#3
+
+$new_arr = array_map("sqrt", $arr);
+
+#4
+#Комибинируем массивы
+$arr = array_combine(range('a', 'z'), range(1, 26));
+
+#5
+
+$str = "1234567890";
+$arr = str_split($str, 2);
+$res = array_sum($arr);
+echo $res."\n";
+
+
+//###########################################################
