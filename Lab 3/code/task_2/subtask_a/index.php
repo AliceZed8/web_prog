d53dcb7e4a29fea7c576c8d1cb67a40f06990a53
AliceZed <zokerfeed@gmail.com>
2024-03-20 06:32:05 +0200

    added Lab3 task 2/subtask A

---
+++ Lab 3/code/task_2/subtask_a/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Subtask A</title>
+</head>
+<body>
+
+    <form id="wordCharacterForm" method="post">
+        <label for="textArea">Введите текст:</label><br>            <!-- Возвращаем тект в текстареа  -->
+        <textarea id="textArea" name="text" rows="4" cols="50"><?php echo isset($_POST['text']) ? $_POST['text'] : "";?></textarea><br><br>
+        <input type="submit" value="Посчтитать кол-во символов и слов">
+    </form>
+    
+    <?php
+    if (isset($_POST['text'])) {
+        $text = $_POST['text'];
+        $characters = mb_strlen($text,'utf-8'); //Cчитаем кол-во символов
+
+        $str = preg_replace("/ {2,}/", ' ', $text); // Заменяем несколько пробелов на один
+        $str = trim($str); // убираем пробелы в начале и в конце
+        $words = count(explode(' ', $str)); //Сплит
+
+        echo "Количество слов: " . $words . "<br>Количество символов:" . $characters;
+    }
+    ?>
+
+</body>
+</html>
